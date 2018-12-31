6c5ccba6f304c8ba9d7490fff6a89a14908ff705
aaronwharton <aircitybruno@gmail.com>
2018-12-31 18:13:45 +0800

    add middleware in streamserver/

--- streamserver/main.go
+++ streamserver/main.go
@@ -5,6 +5,29 @@ import (
 	"net/http"
 )
 
+type middlewareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+func NewMiddlewareHandler(r *httprouter.Router, cc int) http.Handler {
+	m := middlewareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+
+	return m
+}
+
+func (m middlewareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !m.l.GetConn() {
+		sendErrorResponse(w, http.StatusTooManyRequests, "Too many requests!")
+		return
+	}
+
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 
