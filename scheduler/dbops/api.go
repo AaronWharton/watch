11380804bd723e1f0a0ac3d2adbecdd0d4e07b83
aaronwharton <aircitybruno@gmail.com>
2019-01-04 12:06:41 +0800

    update scheduler/ completed

--- scheduler/dbops/api.go
+++ scheduler/dbops/api.go
@@ -1,1 +1,26 @@
 package dbops
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/gommon/log"
+)
+
+func AddVideoDeletionRecord(vid string) error {
+
+
+
+	stmtIns, err := dbConn.Prepare("insert into video_del_rec (video_id) values (?)")
+	if err != nil {
+		return err
+	}
+
+	defer stmtIns.Close()
+
+	_, err = stmtIns.Exec(vid)
+	if err != nil {
+		log.Printf("AddVideoDeletionRecord error: %v", err)
+		return err
+	}
+
+	return nil
+}
